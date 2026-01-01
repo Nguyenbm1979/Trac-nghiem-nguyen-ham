c84156e94d1a95d69b252eca71fd1df68caf940b
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-01 19:51:27 +0700

    Add exam data structure for test paper 01

---
+++ data/de-01.js
@@ -0,0 +1,10 @@
+window.EXAM_DATA = {
+    "title": "Đề Kiểm Tra Nguyên Hàm Số 01",
+    "time": 90, // Số phút
+    "questions": [
+        // DÁN CÁC CÂU HỎI CỦA BẠN VÀO GIỮA ĐÂY
+        { type: 'mcq', q: 'Câu 1: Nguyên hàm của x^3 là:', options: ['x^4/4', '3x^2', 'x^4', '4x^3'], ans: 0, explain: 'CT cơ bản.' },
+        { type: 'mcq', q: 'Câu 2: ...', options: ['A', 'B', 'C', 'D'], ans: 1, explain: '...' },
+        // ... (Copy tiếp các câu khác vào đây)
+    ]
+};
