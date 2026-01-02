20b744ca0e426317d4c401cc09f77c0ae70eee2e
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-02 10:56:42 +0700

    Create list.js

---
+++ data/list.js
@@ -0,0 +1,9 @@
+// DANH SÁCH ĐỀ THI
+// Bạn chỉ cần copy dòng code từ Admin Tool và dán vào trong dấu ngoặc vuông []
+window.EXAM_LIST = [
+    
+    // Ví dụ mẫu (Bạn có thể xóa đi thay bằng đề của bạn)
+    { id: "de-01", title: "Kiểm tra Nguyên Hàm Số 1", subject: "Toan", grade: "12", time: 90 },
+    { id: "de-su12-HK1-25-26", title: "Kiểm tra cuối HK1 Sử 12", subject: "Su", grade: "12", time: 45 },
+
+];
