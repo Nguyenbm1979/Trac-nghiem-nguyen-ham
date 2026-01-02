3c4398bff53756e949dbf2c23f0d8305b4da07f9
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-02 11:36:27 +0700

    Upload exam de-toan12-ontapnguyenham-1-25-26 via AdminTool

---
+++ data/de-toan12-ontapnguyenham-1-25-26.js
@@ -0,0 +1,13 @@
+window.EXAM_DATA = {
+    "title": "Ôn tập nguyên hàm, đề số 1, năm 2025-2026",
+    "subject": "Toan",
+    "grade": "12",
+    "time": 90,
+    "scores": {
+        "mcq": 0.25,
+        "tf": 1,
+        "short": 0.5
+    },
+    "password": "",
+    "questions": []
+};
