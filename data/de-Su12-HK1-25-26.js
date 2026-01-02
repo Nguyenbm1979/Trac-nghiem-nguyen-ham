3c561f84e4d919fe225ab70146f3cb677af0e579
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-02 10:53:46 +0700

    Upload exam de-Su12-HK1-25-26 via AdminTool

---
+++ data/de-Su12-HK1-25-26.js
@@ -0,0 +1,13 @@
+window.EXAM_DATA = {
+    "title": "Cuối Học Kì 1, Năm học 2025-2026",
+    "subject": "Su",
+    "grade": "12",
+    "time": 45,
+    "scores": {
+        "mcq": 0.25,
+        "tf": 1,
+        "short": 0.5
+    },
+    "password": "",
+    "questions": []
+};
