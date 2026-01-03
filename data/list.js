72583dd0aec24c439a859de2c0f4bff12a0ccff0
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-03 10:59:33 +0700

    Update list (Del)

--- data/list.js
+++ data/list.js
@@ -35,13 +35,6 @@ window.EXAM_LIST = [
         "time": 90
     },
     {
-        "id": "de-toan12-ontapnguyenham-1-25-26",
-        "title": "Ôn tập nguyên hàm, đề số 1, năm 2025-2026",
-        "subject": "Toan",
-        "grade": "12",
-        "time": 90
-    },
-    {
         "id": "de-Su12-HK1-25-26",
         "title": "Cuối Học Kì 1, Năm học 2025-2026",
         "subject": "Su",
