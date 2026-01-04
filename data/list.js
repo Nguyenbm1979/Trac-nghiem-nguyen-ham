07e45e6242e72e8ab6aae04d0f0058619880d929
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-04 21:10:13 +0700

    Update list.js

--- data/list.js
+++ data/list.js
@@ -3,7 +3,7 @@ window.EXAM_LIST = [
         "id": "2-Vsat-Deminhhoa-2025",
         "title": "Đề Minh Họa V-Sat, Đại Học Cần Thơ, Năm 2025",
         "subject": "Toan",
-        "grade": "K12",
+        "grade": "12",
         "time": 45
     }
 ];
