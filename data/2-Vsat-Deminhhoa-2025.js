ff5b77798276c1eeb6a4ed04174555ffe839409e
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-04 20:59:41 +0700

    Update exam data for V-Sat 2025

--- data/2-Vsat-Deminhhoa-2025.js
+++ data/2-Vsat-Deminhhoa-2025.js
@@ -1,5 +1,7 @@
 window.EXAM_DATA = {
     "title": "Đề Minh Họa V-Sat, Đại Học Cần Thơ, Năm 2025",
+    "subject": "Toan",
+    "grade": "K12",
     "time": 45,
     "password": "858eabb9c6c80b4947777108ced59f12",
     "encrypted": false,
