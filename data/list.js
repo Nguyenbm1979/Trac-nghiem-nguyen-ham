f4f5ee0b4bf787e8b2680bc7cd6139652c23fc22
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-04 20:57:46 +0700

    Fix subject spelling in exam list

--- data/list.js
+++ data/list.js
@@ -2,7 +2,7 @@ window.EXAM_LIST = [
     {
         "id": "2-Vsat-Deminhhoa-2025",
         "title": "Đề Minh Họa V-Sat, Đại Học Cần Thơ, Năm 2025",
-        "subject": "Toan"
+        "subject": "Toán"
         "grade": "K12"
         "time": 45
     }
