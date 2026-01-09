99f647714fec6dddcc4baaa424f891ebf423616d
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-09 07:50:11 +0700

    Up

--- data/list.js
+++ data/list.js
@@ -1,5 +1,15 @@
 window.EXAM_LIST = [
     {
+        "id": "2-Onnguyenham-de2-25-26",
+        "title": "Đề Ôn Tập Nguyên Hàm, Số 2, Năm 2025-2026",
+        "subject": "Toan",
+        "grade": "12",
+        "time": 60,
+        "allowPrint": false,
+        "start": "2026-01-08T07:08",
+        "end": "2026-01-10T07:40"
+    },
+    {
         "id": "2-Ontichphan-1-25-26",
         "title": "Đề Ôn Tập Tích Phân, Số 1, Năm 2025-2026",
         "subject": "Toan",
@@ -18,16 +28,6 @@ window.EXAM_LIST = [
         "allowPrint": true,
         "start": "2026-01-05T00:01",
         "end": "2026-01-17T21:00"
-    },
-    {
-        "id": "2-Onnguyenham-de2-25-26",
-        "title": "Đề Ôn Tập Nguyên Hàm, Số 2, Năm 2025-2026",
-        "subject": "Toan",
-        "grade": "12",
-        "time": 60,
-        "allowPrint": false,
-        "start": "2026-01-08T07:08",
-        "end": "2026-01-10T07:40"
     },
     {
         "id": "2-De-OnTichPhan-2-25-26",
