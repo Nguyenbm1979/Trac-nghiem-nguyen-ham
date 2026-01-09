577e81e380c58933cabc7e02f4d922be9ff640b3
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-09 08:50:12 +0700

    Up

--- data/list.js
+++ data/list.js
@@ -1,5 +1,15 @@
 window.EXAM_LIST = [
     {
+        "id": "2-GHK2-ThuchanhSP-HCM-25",
+        "title": "Đề thi giữa học kỳ 2, trường THPT, Thực hành sư phạm, HCM, 2025",
+        "subject": "Toan",
+        "grade": "12",
+        "time": 90,
+        "allowPrint": true,
+        "start": "",
+        "end": ""
+    },
+    {
         "id": "2-Ontapnguyenham-1",
         "title": "Đề Ôn Tập Nguyên Hàm, Số 1, Năm 2025-2026",
         "subject": "Toan",
@@ -55,16 +65,6 @@ window.EXAM_LIST = [
         "subject": "Toan",
         "grade": "11",
         "time": 45,
-        "allowPrint": false,
-        "start": "",
-        "end": ""
-    },
-    {
-        "id": "2-GHK2-ThuchanhSP-HCM-25",
-        "title": "Đề thi giữa học kỳ 2, trường THPT, Thực hành sư phạm, HCM, 2025",
-        "subject": "Toan",
-        "grade": "12",
-        "time": 90,
         "allowPrint": false,
         "start": "",
         "end": ""
