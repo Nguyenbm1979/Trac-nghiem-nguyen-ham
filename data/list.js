115403500f6cc28482aa28cbc8b35bc5e0df8e04
Nguyenbm1979 <nbnguyendskhoa14@gmail.com>
2026-01-14 05:17:11 +0700

    Up

--- data/list.js
+++ data/list.js
@@ -1,5 +1,15 @@
 window.EXAM_LIST = [
     {
+        "id": "2-chuyende1-26",
+        "title": "Đề kiểm tra chương 1, Chuyên đề 1, Toán 12, năm 2026",
+        "subject": "Toan",
+        "grade": "12",
+        "time": 30,
+        "allowPrint": false,
+        "start": "",
+        "end": ""
+    },
+    {
         "id": "2-Ontapnguyenham-1",
         "title": "Đề Ôn Tập Nguyên Hàm, Số 1, Năm 2025-2026",
         "subject": "Toan",
